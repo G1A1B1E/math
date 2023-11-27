550adca7204bc5f02da9b7e39173facb6d5703fd
Gabe Macri <gabemacru@gmail.com>
2023-11-27 09:50:38 -0500

    one

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let x = 5
 let y = 10
 let z = 15
 
-function myfunc(){
-    
+const element = document.getElementById("one");
+element.addEventListener("click", myFunc);
+
+
+
+function myFunc(){
+    if (x > 5) {
+       alert('x is greater')
+      }
 }
